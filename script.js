43127088b91fd55000092981180ed5989ff25b18
Kai Xiang <anythinnk@live.com>
2020-03-30 20:04:15 +0800

    Added code for random coloring of cell on hover.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function defineNewHeight(height) {
-    let element = document.querySelector(':root');
+    const element = document.querySelector(':root');
     element.style.setProperty('--sketchpad-height', `${height}`);
 }
 
@@ -12,7 +12,7 @@ function addCell(type, className = '') {
 }
 
 function buildSketchpad(height) {
-    let sketchpadElement = document.querySelector('#sketchpad');
+    const sketchpadElement = document.querySelector('#sketchpad');
     let cell;
     defineNewHeight(height);
     for (let index = 0; index < height**2; index++) {
@@ -21,4 +21,37 @@ function buildSketchpad(height) {
     }
 }
 
+function generateRandomHSL() {
+    let hue, sat, light;
+    hue = Math.floor(Math.random()*360); // 0 to 359
+    sat = Math.floor(Math.random()*71) + 30; // 30 to 100
+    light = Math.floor(Math.random()*41) + 30; // 30 to 70
+    return {hue, sat, light};
+}
+
+function setCellHSL(cell, hue, sat, light) {
+    cell.dataset.hue = `${hue}`;
+    cell.dataset.sat = `${sat}`;
+    cell.dataset.light = `${light}`;
+    cell.style.backgroundColor = `hsl(${hue}, ${sat}%, ${light}%)`;
+    cell.classList.add('colored');
+}
+
+function updateCellColor(cell) {
+    let {hue, sat, light} = generateRandomHSL();
+    if (cell.classList.contains('colored')) {
+        // apply darkening
+    } else {
+    setCellHSL(cell, hue, sat, light);
+    }
+}
+
+function applyHoverFunc() {
+    let cells = document.querySelectorAll('.sketchpad-cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => updateCellColor(cell));
+    })
+}
+
 buildSketchpad(16);
+applyHoverFunc();
